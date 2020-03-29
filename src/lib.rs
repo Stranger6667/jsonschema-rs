3cb0590f9cfb925e5fceb12d87b8fd49e9eb0690
Dmitry Dygalo <dmitry.dygalo@kiwi.com>
2020-03-29 10:58:27 +0200

    docs: Top-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! # jsonschema
+//!
+//! A crate for performing fast JSON Schema validation. It is fast due to schema compilation into
+//! a validation tree, which reduces runtime costs for working with schema parameters.
+//!
+//! Supports:
+//!   - JSON Schema drafts 6, 7 (all test cases);
+//!   - Loading remote documents via HTTP(S);
+//!
+//! ## Example:
+//!
+//! ```rust
+//! use jsonschema::{JSONSchema, Draft};
+//! use serde_json::json;
+//!
+//! fn main() {
+//!     let schema = json!({"maxLength": 5});
+//!     let instance = json!("foo");
+//!     let compiled = JSONSchema::compile(&schema, Some(Draft::Draft7));
+//!     let result = compiled.validate(&instance);
+//!     if let Err(errors) = result {
+//!         for error in errors {
+//!             println!("Validation error: {}", error)
+//!         }   
+//!     }
+//! }
+//! ```
 mod checks;
 mod context;
 mod error;
@@ -15,6 +42,16 @@ pub use validator::JSONSchema;
 extern crate lazy_static;
 
 /// Validates `instance` against `schema`. Draft version is detected automatically.
+/// ```rust
+/// use jsonschema::is_valid;
+/// use serde_json::json;
+///
+/// fn main() {
+///     let schema = json!({"maxLength": 5});
+///     let instance = json!("foo");
+///     assert!(is_valid(&schema, &instance));
+/// }
+/// ```
 pub fn is_valid(schema: &Value, instance: &Value) -> bool {
     let compiled = JSONSchema::compile(schema, None).expect("Invalid schema");
     compiled.is_valid(instance)
