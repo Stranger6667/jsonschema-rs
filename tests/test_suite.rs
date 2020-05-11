953a372ae267d13f1fa615c297f2c2b0e7bcefd0
Samuele Maci <macisamuele@gmail.com>
2020-05-11 18:38:52 +0100

    test: Ensure that draft6 is tested as well

--- tests/test_suite.rs
+++ tests/test_suite.rs
@@ -2,5 +2,5 @@ use draft::test_draft;
 use jsonschema::JSONSchema;
 use serde_json::Value;
 
-//test_draft!("tests/suite/tests/draft6/");
+test_draft!("tests/suite/tests/draft6/");
 test_draft!("tests/suite/tests/draft7/");
