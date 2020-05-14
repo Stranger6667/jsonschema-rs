9ff2dfd727068b52127f84314af0710649b18f78
Samuele Maci <macisamuele@gmail.com>
2020-05-14 12:54:55 +0100

    feat: Add test_to_exclude capability into test_draft procedural macro
    This is needed because there might be integration tests for which we would
    like to ignore the outcome (for example it is an optional test).
    
    In order to achieve this result the procedural macro has been rewritten to exploit
    syn and quote capabilities (enhanced parsing as well as less string concatenations)

--- tests/test_suite.rs
+++ tests/test_suite.rs
@@ -1,5 +1,5 @@
 use draft::test_draft;
 
-test_draft!("tests/suite/tests/draft4/");
+test_draft!("tests/suite/tests/draft4/", {"optional_bignum_0_0", "optional_bignum_2_0"});
 test_draft!("tests/suite/tests/draft6/");
 test_draft!("tests/suite/tests/draft7/");
