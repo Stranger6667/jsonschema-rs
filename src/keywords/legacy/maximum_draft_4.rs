0894934cebccea806243d35682bde7c044079894
Dmitry Dygalo <dadygalo@gmail.com>
2020-05-21 15:52:08 +0200

    docs: Fix markdown syntax

--- src/keywords/legacy/maximum_draft_4.rs
+++ src/keywords/legacy/maximum_draft_4.rs
@@ -1,4 +1,4 @@
-/// Docs: https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-5.1.2
+/// Docs: <https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-5.1.2>
 use crate::{
     compilation::CompilationContext,
     keywords::{exclusive_maximum, maximum, CompilationResult},
--- src/keywords/legacy/minimum_draft_4.rs
+++ src/keywords/legacy/minimum_draft_4.rs
@@ -1,4 +1,4 @@
-/// Docs: https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-5.1.3
+/// Docs: <https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-5.1.3>
 use crate::{
     compilation::CompilationContext,
     keywords::{exclusive_minimum, minimum, CompilationResult},
