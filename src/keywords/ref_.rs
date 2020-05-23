d6cc2249c384ea77cbf1f7b592c231e1da592fe3
Samuele Maci <macisamuele@gmail.com>
2020-05-23 13:46:30 +0100

    perf: $ref validator compiles validators only once and not at every validation request

--- src/keywords/ref_.rs
+++ src/keywords/ref_.rs
@@ -1,60 +1,78 @@
 use super::{CompilationResult, Validate};
 use crate::{
     compilation::{compile_validators, CompilationContext, JSONSchema},
-    error::{error, ErrorIterator},
+    error::{error, ErrorIterator, ValidationError},
+    keywords::Validators,
 };
+use parking_lot::RwLock;
 use serde_json::Value;
 use url::Url;
 
 pub struct RefValidator {
     reference: Url,
+    /// Precomputed validators.
+    /// They are behind a RwLock as is not possible to compute them
+    /// at compile time without risking infinite loops of references
+    /// and at the same time during validation we iterate over shared
+    /// references (&self) and not owned references (&mut self).
+    validators: RwLock<Option<Validators>>,
 }
 
 impl RefValidator {
     #[inline]
     pub(crate) fn compile(reference: &str, context: &CompilationContext) -> CompilationResult {
         let reference = context.build_url(reference)?;
-        Ok(Box::new(RefValidator { reference }))
+        Ok(Box::new(RefValidator {
+            reference,
+            validators: RwLock::new(None),
+        }))
+    }
+
+    /// Ensure that validators are built and built once.
+    fn ensure_validators<'a>(&self, schema: &'a JSONSchema) -> Result<(), ValidationError<'a>> {
+        if self.validators.read().is_none() {
+            let (scope, resolved) =
+                schema
+                    .resolver
+                    .resolve_fragment(schema.draft, &self.reference, schema.schema)?;
+            let context = CompilationContext::new(scope, schema.draft);
+            let validators = compile_validators(&resolved, &context)?;
+
+            // Inject the validators into self.validators
+            *self.validators.write() = Some(validators);
+        }
+        Ok(())
     }
 }
 
 impl Validate for RefValidator {
     fn validate<'a>(&self, schema: &'a JSONSchema, instance: &'a Value) -> ErrorIterator<'a> {
-        match schema
-            .resolver
-            .resolve_fragment(schema.draft, &self.reference, schema.schema)
-        {
-            Ok((scope, resolved)) => {
-                let context = CompilationContext::new(scope, schema.draft);
-                match compile_validators(&resolved, &context) {
-                    Ok(validators) => Box::new(
-                        validators
-                            .into_iter()
-                            .flat_map(move |validator| validator.validate(schema, instance)),
-                    ),
-                    Err(e) => error(e.into()),
-                }
-            }
-            Err(e) => error(e),
+        if let Err(err) = self.ensure_validators(schema) {
+            error(err)
+        } else {
+            Box::new(
+                self.validators
+                    .read()
+                    .as_ref()
+                    .expect("ensure_validators guarantees the presence of the validators")
+                    .iter()
+                    .flat_map(move |validator| validator.validate(schema, instance))
+                    .collect::<Vec<_>>()
+                    .into_iter(),
+            )
         }
     }
 
     fn is_valid(&self, schema: &JSONSchema, instance: &Value) -> bool {
-        match schema
-            .resolver
-            .resolve_fragment(schema.draft, &self.reference, schema.schema)
-        {
-            Ok((scope, resolved)) => {
-                let context = CompilationContext::new(scope, schema.draft);
-                match compile_validators(&resolved, &context) {
-                    Ok(validators) => validators
-                        .into_iter()
-                        .all(move |validator| validator.is_valid(schema, instance)),
-
-                    Err(_) => false,
-                }
-            }
-            Err(_) => false,
+        if self.ensure_validators(schema).is_err() {
+            false
+        } else {
+            self.validators
+                .read()
+                .as_ref()
+                .expect("ensure_validators guarantees the presence of the validators")
+                .iter()
+                .all(move |validator| validator.is_valid(schema, instance))
         }
     }
 
