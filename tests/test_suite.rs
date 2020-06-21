a9297963d9e78028df82dab7d64b8d436be999db
Samuele Maci <macisamuele@gmail.com>
2020-06-21 22:56:34 +0100

    chore: Disable/Ignore idn-hostname tests (#101)

--- tests/test_suite.rs
+++ tests/test_suite.rs
@@ -1,5 +1,12 @@
 use draft::test_draft;
 
-test_draft!("tests/suite/tests/draft4/", {"optional_bignum_0_0", "optional_bignum_2_0"});
+test_draft!("tests/suite/tests/draft4/", {
+    "optional_bignum_0_0",
+    "optional_bignum_2_0",
+});
 test_draft!("tests/suite/tests/draft6/");
-test_draft!("tests/suite/tests/draft7/");
+test_draft!("tests/suite/tests/draft7/", {
+    "optional_format_idn_hostname_0_11", // https://github.com/Stranger6667/jsonschema-rs/issues/101
+    "optional_format_idn_hostname_0_6",  // https://github.com/Stranger6667/jsonschema-rs/issues/101
+    "optional_format_idn_hostname_0_7",  // https://github.com/Stranger6667/jsonschema-rs/issues/101
+});
