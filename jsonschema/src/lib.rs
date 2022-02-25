9d391bff7cdcaa575e6f456c4b85a832502d3234
Dmitry Dygalo <dmitry@dygalo.dev>
2022-02-25 15:03:03 +0100

    test: Add a test case for #343

--- jsonschema/src/lib.rs
+++ jsonschema/src/lib.rs
@@ -237,4 +237,19 @@ mod tests {
         let schema = json!({"pattern": "\\u"});
         assert!(JSONSchema::compile(&schema).is_err())
     }
+
+    #[test]
+    fn issue_343() {
+        let file = std::fs::File::open("schema.json").unwrap();
+        let reader = std::io::BufReader::new(file);
+        let schema = serde_json::from_reader(reader).unwrap();
+
+        let file = std::fs::File::open("instance.json").unwrap();
+        let reader = std::io::BufReader::new(file);
+        let instance = serde_json::from_reader(reader).unwrap();
+
+        let compiled = JSONSchema::compile(&schema).expect("A valid schema");
+        let result = compiled.validate(&instance);
+        assert!(result.is_ok())
+    }
 }
