cafe742ad100cba95f3603fd2700cf0d27fdcabe
Dmitry Dygalo <dmitry@dygalo.dev>
2024-09-29 16:43:47 +0200

    chore: Update fluent-uri to 0.3.1
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- crates/jsonschema-referencing/src/uri.rs
+++ crates/jsonschema-referencing/src/uri.rs
@@ -3,29 +3,12 @@ use fluent_uri::{encoding::EString, Uri, UriRef};
 use crate::Error;
 pub use fluent_uri::encoding::encoder::Path;
 
-const fn table() -> [bool; 259] {
-    let mut bytes = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-.".as_slice();
-    let mut table = [false; 259];
-    while let [cur, rem @ ..] = bytes {
-        table[*cur as usize] = true;
-        bytes = rem;
-    }
-    table
-}
-
-const SCHEME: &[bool; 259] = &table();
 /// Resolves the URI reference against the given base URI and returns the target URI.
 ///
 /// # Errors
 ///
 /// Returns an error if base has not schema or there is a fragment.
 pub fn resolve_against(base: &UriRef<&str>, uri: &str) -> Result<UriRef<String>, Error> {
-    // Emulate `ParseError` to avoid panic on invalid input.
-    //
-    // See https://github.com/yescallop/fluent-uri-rs/issues/28
-    if base.as_str().is_empty() || !base.as_str().bytes().any(|x| !SCHEME[x as usize]) {
-        Uri::try_from("/".to_string()).map_err(|err| err.strip_input())?;
-    }
     Ok(UriRef::parse(uri)?
         .resolve_against(&(Uri::try_from(*base)?))?
         .normalize()
